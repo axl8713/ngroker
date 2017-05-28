6b5aa4acde69a8d6fade9f40380fbef8f5529cba
aleric <ale.ricchiuti@hotmail.it>
2017-05-28 12:28:13 +0200

    Bugfix.

--- main/ngroker.py
+++ main/ngroker.py
@@ -16,18 +16,15 @@ logger = logging.getLogger(__name__)
 
 class Tunnel(object):
     def __init__(self, protocol=None, port=None, name=None, public_url=None):
-    self.name = name
-    self.port = port
-    self.public_url = public_url
-    self.protocol = protocol
+        self.name = name
+        self.port = port
+        self.public_url = public_url
+        self.protocol = protocol
 
     def __repr__(self):
         return "Tunnel " + self.name + " " + self.public_url if self.public_url != None else "Tunnel not open yet."
 
 class Ngroker(object):
-
-    def __init__(self):
-    print Ngroker_Config.ngrok_api_url
 
     is_ngrok_started = False
 
