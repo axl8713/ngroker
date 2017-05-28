b70db341d7981e28d6b0c8b19ab443e155b5faf6
aleric <ale.ricchiuti@hotmail.it>
2017-05-28 12:15:31 +0200

    Refactoring.

--- main/ngroker.py
+++ main/ngroker.py
@@ -8,56 +8,48 @@ import logging
 import requests
 import subprocess
 import time
-from distutils.spawn import find_executable
-import ConfigParser
 import os
+from distutils.spawn import find_executable
+from ngroker_config import Ngroker_Config
 
 logger = logging.getLogger(__name__)
-# api_url = "http://localhost:4040/api/tunnels"
-# ngrok_auth_token = '3peXR86bpzfjMwDfFRK4z_4v8QM9SwRFogk1EGbCGGf'
-# ngrok_exec_path = "/home/aleric/Desktop/ngrok"
 
 class Tunnel(object):
-    def __init__(
-            self, protocol=None, port=None, name=None, public_url=None):
-        self.name = name
-        self.port = port
-        self.public_url = public_url
-        self.protocol = protocol
+    def __init__(self, protocol=None, port=None, name=None, public_url=None):
+    self.name = name
+    self.port = port
+    self.public_url = public_url
+    self.protocol = protocol
 
     def __repr__(self):
-        return "Tunnel " + self.name + " " + self.public_url if self.public_url != None else "<pending Tunnel>"
+        return "Tunnel " + self.name + " " + self.public_url if self.public_url != None else "Tunnel not open yet."
 
 class Ngroker(object):
 
-    is_ngrok_started = False
-
     def __init__(self):
-        self._config = ConfigParser.ConfigParser()
-        self._config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), "ngroker.ini"))
+    print Ngroker_Config.ngrok_api_url
 
-        self._api_url = self._config.get('ngrok','api_url')
-        _ngrok_auth_token = self._config.get('ngrok','auth_token')
-
+    is_ngrok_started = False
 
     def start(self):
 
         if not self.is_ngrok_started:
             try:
-                ngrok_exec_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"ngrok")
+                ngrok_exec_path = os.path.join(Ngroker_Config.exec_path,"ngrok")
                 self._ngrok = subprocess.Popen(
-                    [ngrok_exec_path, "start", "-config="+os.path.join(os.path.dirname(os.path.abspath(__file__)),"ngrok.yml"), "--none", "-region=eu"],
-                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                [ngrok_exec_path, "start", "-config="+os.path.join(Ngroker_Config.exec_path,"ngrok.yml"), "--none", "-region=eu"],
+                stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             except OSError as oserr:
                 raise IOError("ngrok executable not found.")
 
-            time.sleep(1);
+        # maybe not needed
+        time.sleep(1);
 
-            if self._ngrok.poll() is None:
-                self.is_ngrok_started = True
-                logger.info("ngrok started.")
-            else:
-                raise ValueError("ngrok is not running.")
+        if self._ngrok.poll() is None:
+            self.is_ngrok_started = True
+            logger.info("ngrok started.")
+        else:
+            raise ValueError("ngrok is not running.")
 
     def stop(self):
         if self._ngrok.poll() is None:
@@ -72,8 +64,8 @@ class Ngroker(object):
 
         logger.debug("Opening ngrok %s tunnel '%s' for port %d", protocol, name, port)
 
-        #disabilitare inspection!
-        post = requests.post(self._api_url, json = {'addr':str(port), 'proto':str(protocol), 'name':str(name)})
+        # TODO: disable inspection!
+        post = requests.post(Ngroker_Config.ngrok_api_url, json = {'addr':str(port), 'proto':str(protocol), 'name':str(name)})
 
         try:
             post.raise_for_status()
@@ -89,7 +81,7 @@ class Ngroker(object):
     def close_tunnel(self, name):
 
         logger.debug("Closing ngrok tunnel '%s'", name)
-        delete = requests.delete(self._api_url + "/" + str(name))
+        delete = requests.delete(Ngroker_Config.ngrok_api_url + "/" + str(name))
 
         try:
             delete.raise_for_status()
--- main/telegram_bot.py
+++ main/telegram_bot.py
@@ -2,95 +2,119 @@ import ngroker
 import telepot
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 import time
+from ngroker_config import Ngroker_Config
 import logging
-import ConfigParser
 import os
 
 logger = logging.getLogger(__name__)
 
 class Telegram_bot(object):
 
+    _bot_command_start = "/start"
+    _bot_command_open_tunnel = "/open_tunnel"
+    _bot_command_new_tunnel = "/new_tunnel"
+    _bot_command_stop = '/stop'
+
+    _callback_data_ssh = 'ssh'
+    _callback_data_manager = 'manager'
+    _callback_data_tcp = 'tcp'
+    _callback_data_http = 'http'
+
+    _predefined_manager_tunnel_data = {"protocol":'http', "port": 4040, "name": "ngrok manager"}
+    _predefined_ssh_tunnel_data = {"protocol": "tcp", "port": 22, "name": "SSH"}
+
     def __init__(self):
 
-        self._config = ConfigParser.ConfigParser()
-        self._config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)),"ngroker.ini"))
+       self._bot = telepot.Bot(Ngroker_Config.telegram_bot_token)
+       self._bot.message_loop({'chat': self.handle_message, 'callback_query': self.on_callback_query})
 
-        self._bot = telepot.Bot(self._config.get('telegram','bot_token'))
-        self._bot.message_loop({'chat': self.handle_message, 'callback_query': self.on_callback_query})
-
-        self._ngrok_client = ngroker.Ngroker()
-        self._ngrok_client.start()
+       self._ngrok_client = ngroker.Ngroker()
+       self._ngrok_client.start()
 
     def handle_message(self, message):
+
         content_type, chat_type, chat_id = telepot.glance(message)
 
         try:
             if content_type == 'text':
 
-                command = message['text'].upper();
+                command = message['text'];
 
                 logger.debug("%s command received", command)
 
-                if command == '/START':
+                if command == self._bot_command_start:
+                    self.handle_start_command(chat_id)
 
-                    self._ngrok_client.start()
+                elif command == self._bot_command_open_tunnel:
+                    self.handle_open_tunnel_command(chat_id)
 
-                    keyboard = ReplyKeyboardMarkup(keyboard=[
-                                     ['/open', '/stop'],
-                    ])
-                    self._bot.sendMessage(chat_id, 'Select a command', reply_markup=keyboard)
+                elif command == self._bot_command_new_tunnel:
+                    self.handle_new_tunnel_command(chat_id)
 
-                elif command == '/OPEN':
-                    if self._ngrok_client.is_ngrok_started:
-                        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                            [
-                                InlineKeyboardButton(text='SSH', callback_data='ssh'),
-                                InlineKeyboardButton(text='HTTP', callback_data='http'),
-                                InlineKeyboardButton(text='manager', callback_data='manager_http')
-                            ],
-                        ])
-                        self._bot.sendMessage(chat_id, 'Select tunnel type', reply_markup=keyboard)
-                    else:
-                        self._bot.sendMessage(chat_id, 'please start client first via /start')
+                elif command == self._bot_command_stop:
+                    self.handle_stop_command(chat_id)
+                else:
+                    raise Exception("command not recognized")
 
-                elif command == '/STOP':
-                    self._ngrok_client.stop()
-                    keyboard = ReplyKeyboardMarkup(keyboard=[
-                                     ['/start'],
-                    ])
-                    self._bot.sendMessage(chat_id, 'Select /start command to start client', reply_markup=keyboard)
         except Exception as ex:
-            logger.error("error in handling message: "+ str(ex))
-            self._bot.sendMessage(chat_id, 'someting went wrong: '+ str(ex))
+            logger.error("error in handling message: " + str(ex))
+            self._bot.sendMessage(chat_id, "someting went wrong: " + str(ex))
 
-    def on_callback_query(self, msg):
-        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    def handle_start_command(self, chat_id):
+        self._ngrok_client.start()
+        keyboard = ReplyKeyboardMarkup(keyboard=[
+                         [self._bot_command_open_tunnel, self._bot_command_new_tunnel],
+                         [self._bot_command_stop]
+        ])
+        self._bot.sendMessage(chat_id, 'Select a command', reply_markup=keyboard)
 
-        self._bot.answerCallbackQuery(query_id, text='opening ' + query_data + ' tunnel. stay tight.')
+    def handle_open_tunnel_command(self, chat_id):
+        if self._ngrok_client.is_ngrok_started:
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [
+                    InlineKeyboardButton(text=self._predefined_ssh_tunnel_data["name"], callback_data=self._callback_data_ssh),
+                    InlineKeyboardButton(text=self._predefined_manager_tunnel_data["name"], callback_data=self._callback_data_manager)
+                ],
+            ])
+            self._bot.sendMessage(chat_id, 'Please select the tunnel to be opened.', reply_markup=keyboard)
+        else:
+            self.send_please_start_ngrok_message(chat_id)
 
-        if query_data=='manager_http':
-            tunnel = self.open_manager_tunnel()
-        elif query_data=='ssh':
-            tunnel = self.open_ssh_tunnel()
+    def send_please_start_ngrok_message(self, chat_id):
+        self._bot.sendMessage(chat_id, 'Please start client first via ' + self._bot_command_start + ' command.')
 
+    def handle_new_tunnel_command(self, chat_id):
+        if self._ngrok_client.is_ngrok_started:
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [
+                    InlineKeyboardButton(text='TCP', callback_data=self._callback_data_tcp),
+                    InlineKeyboardButton(text='HTTP', callback_data=self._callback_data_http),
+                ],
+            ])
+            self._bot.sendMessage(chat_id, 'Please select a protocol for the tunnel.', reply_markup=keyboard)
+        else:
+            self.send_please_start_ngrok_message(chat_id)
+
+    def handle_stop_command(self, chat_id):
+        self._ngrok_client.stop()
+        keyboard = ReplyKeyboardMarkup(keyboard=[
+                         [self._bot_command_start],
+        ])
+        self._bot.sendMessage(chat_id, 'Select ' + self._bot_command_start + ' command to start client', reply_markup=keyboard)
+
+    def on_callback_query(self, message):
+        query_id, from_id, query_data = telepot.glance(message, flavor='callback_query')
+
+        if query_data == self._callback_data_manager:
+            self.open_predefined_tunnel(self._predefined_manager_tunnel_data, query_id, from_id)
+
+        elif query_data == self._callback_data_ssh:
+            self.open_predefined_tunnel(self._predefined_ssh_tunnel_data, query_id, from_id)
+
+        # elif query_data == self._callback_data_tcp:
+
+    def open_predefined_tunnel(self, tunnel_data, query_id, from_id):
+
+        self._bot.answerCallbackQuery(query_id, text='Opening ' + tunnel_data["name"] + ' tunnel. Stay tight.')
+        tunnel = self._ngrok_client.open_tunnel(tunnel_data["protocol"], tunnel_data["port"], tunnel_data["name"])
         self._bot.sendMessage(from_id, str(tunnel))
-
-    def open_manager_tunnel(self):
-
-        protocol = 'http'
-        port = 4040
-        name = 'ngrok_manager'
-
-        t = self._ngrok_client.open_tunnel(protocol, port, name)
-        print t
-        return t
-
-    def open_ssh_tunnel(self):
-
-        protocol = 'tcp'
-        port = 22
-        name = 'raspizero-ssh'
-
-        tunnel = self._ngrok_client.open_tunnel(protocol, port, name)
-        print tunnel
-        return tunnel
